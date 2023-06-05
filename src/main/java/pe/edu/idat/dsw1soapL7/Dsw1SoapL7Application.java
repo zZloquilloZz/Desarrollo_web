626bff503b7f0ddb80900409155397ff68248cf3
ademir <ademir_fernandez_hernandez03@outlook.com>
2023-06-05 08:06:56 -0500

    Modificaciones

---
+++ src/main/java/pe/edu/idat/dsw1soapL7/Dsw1SoapL7Application.java
@@ -0,0 +1,12 @@
+package pe.edu.idat.dsw1soapL7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsw1SoapL7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsw1SoapL7Application.class, args);
+	}
+}
---
+++ src/test/java/pe/edu/idat/dsw1soapL7/Dsw1SoapL7ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.dsw1soapL7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsw1SoapL7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
