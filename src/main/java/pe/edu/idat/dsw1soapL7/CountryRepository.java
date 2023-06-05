795345bce9223a3a9a1e932a42ed09abd9b75641
ademir <ademir_fernandez_hernandez03@outlook.com>
2023-06-05 09:16:23 -0500

    ec1

--- src/main/java/pe/edu/idat/dsw1soapL7/CountryRepository.java
+++ src/main/java/pe/edu/idat/dsw1soapL7/CountryRepository.java
@@ -16,28 +16,53 @@ public class CountryRepository {
     @PostConstruct
     public void initData() {
         Country spain = new Country();
-        spain.setName("Spain");
-        spain.setCapital("Madrid");
-        spain.setCurrency(Currency.EUR);
+        spain.setName("Lunes");
+        spain.setCapital("Monday");
         spain.setPopulation(46704314);
 
         countries.put(spain.getName(), spain);
 
         Country poland = new Country();
-        poland.setName("Poland");
-        poland.setCapital("Warsaw");
-        poland.setCurrency(Currency.PLN);
+        poland.setName("Martes");
+        poland.setCapital("Tuesday");
         poland.setPopulation(38186860);
 
         countries.put(poland.getName(), poland);
 
         Country uk = new Country();
-        uk.setName("United Kingdom");
-        uk.setCapital("London");
-        uk.setCurrency(Currency.GBP);
+        uk.setName("Miercoles");
+        uk.setCapital("Wednesday");
         uk.setPopulation(63705000);
 
         countries.put(uk.getName(), uk);
+
+        Country lunes = new Country();
+        lunes.setName("Jueves");
+        lunes.setCapital("Thursday");
+        lunes.setPopulation(63705000);
+
+        countries.put(lunes.getName(), lunes);
+
+        Country vienes = new Country();
+        vienes.setName("Viernes");
+        vienes.setCapital("Friday");
+        vienes.setPopulation(63705000);
+
+        countries.put(vienes.getName(), vienes);
+
+        Country sabado = new Country();
+        sabado.setName("Sabado");
+        sabado.setCapital("Saturday");
+        sabado.setPopulation(63705000);
+
+        countries.put(sabado.getName(), sabado);
+
+        Country domingo = new Country();
+        domingo.setName("Domingo");
+        domingo.setCapital("Sunday");
+        domingo.setPopulation(63705000);
+
+        countries.put(domingo.getName(), domingo);
     }
 
     public Country findCountry(String name) {
